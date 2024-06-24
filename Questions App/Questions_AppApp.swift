89ca93297f95ceabe9d225ac0ffa15165577799e
Jada <jadahernandez0828@gmail.com>
2024-06-24 11:17:41 -0400

    Initial Commit

---
+++ Questions App/Questions_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Questions_AppApp.swift
+//  Questions App
+//
+//  Created by Scholar on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Questions_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
